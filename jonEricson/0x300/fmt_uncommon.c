72390885d6c9a5bcbbd4243043bd6e000ae8bbb8
adib <adibikar@gmail.com>
2017-03-26 02:10:09 -0400

    Format parameter

--- jonEricson/0x300/fmt_uncommon.c
+++ jonEricson/0x300/fmt_uncommon.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-	int A = 5, B = 7, count_one, count_two;
+	unsigned int A = 5, B = 7, count_one, count_two;
 
-	printf("Muhammad Adib Dzulfikar %n Muhammad Adib Dzulfikar %n\n",
-			&count_one, &count_two);
+	printf("ZZZZZ%nZZZZZ%n\n", &count_one, &count_two);
 	printf("count_one: %d\n", count_one);
 	printf("count_two: %d\n", count_two);
-	printf("A is %d and is at %08x. B is %d.\n", A, &A, B);
+	printf("A is %d and is at %p. B is %d.\n", A, &A, B);
 	exit(0);
 }
