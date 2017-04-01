0f8ebedafe7dcb2bb6d44cedf35d83d98c59e686
adib <adibikar@gmail.com>
2017-04-01 12:56:35 -0400

    How printf works on stack

--- jonEricson/0x300/howprintfwork.c
+++ jonEricson/0x300/howprintfwork.c
@@ -4,12 +4,17 @@
 
 void howprintfworks();
 
+static int val = 16;
+
 int main(int argc, char *argv[]) {
 	if(argc < 2) {
 		printf("Usage: %s <text to print>\n", argv[0]);
 		exit(0);
 	}
+	printf("&val: %p\n", &val);
+	printf("[before] val: %d\n", val);
 	howprintfworks(argv[1]);
+	printf("[after] val: %d | val: 0x%08x\n", val, val);
 	return 0;
 }
 
